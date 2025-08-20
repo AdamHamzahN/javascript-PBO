fd4beb585f8e1734a7cb0f208250ac2c5ef164d8
Adam Hamzah Nurwahyudi <adamhamzahofficial13@gmail.com>
2025-08-20 17:06:13 +0700

    update

--- inheritance.js
+++ inheritance.js
@@ -29,18 +29,19 @@
 // child.parentSay();
 
 class LuasBangunDatar {
-    constructor(sisi, panjang, lebar, alas, tinggi) {
+    constructor({ sisi, panjang, lebar, alas, tinggi, radius }) {
         this.sisi = sisi;
         this.panjang = panjang;
         this.lebar = lebar;
         this.alas = alas;
         this.tinggi = tinggi;
+        this.radius = radius;
     }
 }
 
 class Persegi extends LuasBangunDatar {
     constructor(sisi) {
-        super(sisi);
+        super({ sisi: sisi })
     }
 
     hitungLuas() {
@@ -51,7 +52,7 @@ class Persegi extends LuasBangunDatar {
 
 class PersegiPanjang extends LuasBangunDatar {
     constructor(panjang, lebar) {
-        super(panjang, lebar);
+        super({ panjang: panjang, lebar: lebar });
     }
 
     hitungLuas() {
@@ -61,20 +62,51 @@ class PersegiPanjang extends LuasBangunDatar {
 }
 
 class Segitiga extends LuasBangunDatar {
+    constructor(alas, tinggi) {
+        super({ alas: alas, tinggi: tinggi });
+    }
+
+    hitungLuas() {
+        this.luas = this.alas * this.tinggi / 2;
+        console.log(`Luas segitiga dengan alas ${this.alas} dan tinggi ${this.tinggi} adalah : ${this.luas}`)
+    }
 
 }
 
 class Lingkaran extends LuasBangunDatar {
+    constructor(radius) {
+        super({ radius : radius });
+    }
 
+    hitungLuas() {
+        this.luas = 3.14 * this.radius ** 2;
+        console.log(`Luas Lingkaran dengan radius ${this.radius} adalah : ${this.luas}`)
+    }
 }
 
 class Jajargenjang extends LuasBangunDatar {
+    constructor(alas, tinggi) {
+        super({ alas: alas, tinggi: tinggi });
+    }
 
+    hitungLuas() {
+        this.luas = this.alas * this.tinggi;
+        console.log(`Luas Jajargenjang dengan alas ${this.alas} dan tinggi ${this.tinggi} adalah : ${this.luas}`)
+    }
 }
 
 // instasiasi
-const persegi = new Persegi(2);
-persegi.hitungLuas();
+const persegi = new Persegi(6);
+persegi.hitungLuas()
 
-const persegiPanjang = new PersegiPanjang(2,4);
+const persegiPanjang = new PersegiPanjang(2, 4);
 persegiPanjang.hitungLuas();
+
+const lingkaran = new Lingkaran(10);
+lingkaran.hitungLuas();
+
+const segitiga = new Segitiga(10, 7)
+segitiga.hitungLuas();
+
+const jajargenjang = new Jajargenjang(10, 7);
+jajargenjang.hitungLuas();
